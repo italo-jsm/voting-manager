a6a059d9463b599b8472feed01e5cc36459378d9
italomariano <italo.mariano@inventcloud.com.br>
2024-02-17 19:08:44 -0300

    initial-setup

---
+++ src/main/java/com/italo/votingmanager/VotingManagerApplication.java
@@ -0,0 +1,13 @@
+package com.italo.votingmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VotingManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VotingManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/italo/votingmanager/VotingManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.italo.votingmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VotingManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
